fd01f8baa434feef58d457a6776aa1f7055b3fe2
Ola <defto94@gmail.com>
2018-03-17 22:49:08 +0100

    Spring Security, Sheduled Task & Flyway

---
+++ src/main/java/pl/lodz/project/task1/Application.java
@@ -0,0 +1,26 @@
+package pl.lodz.project.task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : index
+    Created on : 2018-01-15, 20:44:47
+    Author     : defto
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <div>
+            <br/>
+            Welcome!
+        </div>
+    </body>
+</html>
